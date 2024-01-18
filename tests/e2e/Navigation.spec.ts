a173afbaa3f95236c2bbbb6ee281a045db5945c5
TranTheDang <thedangqn@gmail.com>
2024-01-18 19:10:55 +0700

    test(comment): comment all test cases

--- tests/e2e/Navigation.spec.ts
+++ tests/e2e/Navigation.spec.ts
@@ -1,27 +1,24 @@
-// test.describe('Navigation', () => {
-//   test.describe('Static pages', () => {
-//     test('should take screenshot of the homepage', async ({ page }) => {
-//       await page.goto('/');
+import { test } from '@playwright/test';
 
-//       await expect(
-//         page.getByRole('heading', {
-//           name: 'Boilerplate code for your Nextjs project with Tailwind CSS',
-//         }),
-//       ).toBeVisible();
-
-//       await percySnapshot(page, 'Homepage');
-//     });
-
-//     test('should take screenshot of the about page', async ({ page }) => {
-//       await page.goto('/about');
-
-//       await expect(
-//         page.getByRole('link', {
-//           name: 'About',
-//         }),
-//       ).toBeVisible();
-
-//       await percySnapshot(page, 'About');
-//     });
-//   });
-// });
+test.describe('Navigation', () => {
+  // test.describe('Static pages', () => {
+  //   test('should take screenshot of the homepage', async ({ page }) => {
+  //     await page.goto('/');
+  //     await expect(
+  //       page.getByRole('heading', {
+  //         name: 'Boilerplate code for your Nextjs project with Tailwind CSS',
+  //       }),
+  //     ).toBeVisible();
+  //     await percySnapshot(page, 'Homepage');
+  //   });
+  //   test('should take screenshot of the about page', async ({ page }) => {
+  //     await page.goto('/about');
+  //     await expect(
+  //       page.getByRole('link', {
+  //         name: 'About',
+  //       }),
+  //     ).toBeVisible();
+  //     await percySnapshot(page, 'About');
+  //   });
+  // });
+});
