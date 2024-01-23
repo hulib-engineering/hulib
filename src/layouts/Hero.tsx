3a1715b4c7bb3424b7d5f5c5d16024277c3696cc
JtotheDoubleO <joo291998@gmail.com>
2024-01-24 00:01:18 +0700

    feat(Hero): added layout, rich text helper and intl phrases

--- src/layouts/Hero.tsx
+++ src/layouts/Hero.tsx
@@ -1,134 +1,154 @@
 'use client';
 
 import Image from 'next/image';
-import React from 'react';
+import { useTranslations } from 'next-intl';
+import React, { type ReactNode } from 'react';
 
-// import heroArtwork from '../../public/assets/images/Hero-artwork.png';
+import { mergeClassnames } from '@/components/private/utils';
+
+const StarIcons = [
+  { size: 70, yPosition: 'top-60', xPosition: 'right-28' },
+  { size: 35, yPosition: 'top-[26rem]', xPosition: 'right-16' },
+  { size: 35, yPosition: 'top-24', xPosition: 'left-60' },
+];
+const VectorIcons = [
+  { width: 57, height: 75, yPosition: 'top-[22rem]', xPosition: 'left-0' },
+  { width: 49, height: 65, yPosition: 'top-20', xPosition: 'right-0' },
+  { width: 36, height: 53, yPosition: 'top-40', xPosition: 'left-32' },
+];
+
+const customMessage = (font: string): (() => ReactNode) => {
+  // eslint-disable-next-line react/display-name
+  return (...chunks: ReactNode[]): ReactNode => {
+    return <span className={font}>{chunks}</span>;
+  };
+};
 
 const Hero = () => {
+  const t = useTranslations('Index');
+
   return (
-    <section className="flex flex-col items-center justify-center gap-8 self-stretch px-[5.625rem] pb-48 pt-8">
+    <section className="flex flex-col items-center justify-center gap-32 self-stretch px-[5.625rem] pb-[5.625rem] pt-8">
       <div className="flex w-[48rem] flex-col items-center gap-6 pb-3">
         <div className="flex flex-col items-center gap-4 self-stretch">
-          <p className="!my-0 self-stretch text-center text-lg font-medium uppercase leading-[1.6875rem] text-[#0061EF]">
-            a social project
+          <p className="self-stretch text-center text-lg font-medium uppercase leading-[1.6875rem] text-primary">
+            {t('hero_title')}
           </p>
-          <h1 className="w-[41.3125rem] text-center text-[5rem] font-semibold leading-normal text-[#002254]">
-            we are here to support you
+          <h1 className="text-center text-[5rem] font-semibold capitalize leading-normal text-slate-1000">
+            {t('hero_motto')}
           </h1>
-          <p className="!my-0 self-stretch text-center text-2xl font-light leading-9 text-[#002254] opacity-50">
-            There is a story and experience within each of us, so we are born to
-            be somebody.
+          <p className="self-stretch text-center text-2xl font-light leading-9 text-slate-1000 opacity-50">
+            {t('hero_description')}
           </p>
         </div>
         <div className="flex flex-col items-center gap-2.5">
           <button
             type="button"
-            className="flex h-12 items-center justify-center gap-1 rounded-[2rem] bg-[#0061EF] px-8 py-3"
+            className="flex h-12 items-center justify-center gap-1 rounded-[2rem] bg-primary px-8 py-3"
           >
-            <p className="!my-0 text-base font-medium leading-6 text-white">
-              LEARN MORE
+            <p className="text-base font-medium uppercase leading-6 text-white">
+              {t('hero_call_to_action')}
             </p>
             <Image
               width={24}
               height={24}
-              src="/assets/icons/download-icon.svg"
-              alt="download-icon"
+              src="/assets/images/icons/download-icon.svg"
+              alt="Download icon"
+              loading="lazy"
             />
           </button>
         </div>
       </div>
-      <div className="relative h-[27.75rem] w-[78.75rem]">
-        <div className="relative flex h-[31.9375rem] w-[77.9375rem] shrink-0 justify-center ">
+      <div className="relative w-full">
+        <div className="relative flex h-[31.9375rem] w-[77.9375rem] shrink-0 justify-center">
           <Image
-            alt="Hero Artwork"
+            alt="Hero artwork"
             src="/assets/images/hero-artwork.png"
-            className="h-[32rem] w-[50rem] shrink-0"
+            className="shrink-0"
+            width={748}
+            height={511}
             priority
-            fill
+            // fill
           />
-          <Image
-            width={70}
-            height={70}
-            alt="Hero Star 1"
-            src="/assets/icons/hero/Hero-star.svg"
-            className="absolute right-28 top-60 shrink-0"
-          />
-          <Image
-            width={35}
-            height={35}
-            alt="Hero Star 2"
-            src="/assets/icons/hero/Hero-star.svg"
-            className="absolute right-16 top-[26rem] shrink-0"
-          />
-          <Image
-            width={35}
-            height={35}
-            alt="Hero Star 3"
-            src="/assets/icons/hero/Hero-star.svg"
-            className="absolute left-60 top-24 shrink-0"
-          />
-          <Image
-            width={57}
-            height={75}
-            alt="Hero Vector 1"
-            src="/assets/icons/hero/Hero-vector1.svg"
-            className="absolute left-0 top-[22rem] shrink-0"
-          />
-          <Image
-            width={49}
-            height={65}
-            alt="Hero Vector 2"
-            src="/assets/icons/hero/Hero-vector2.svg"
-            className="absolute right-0 top-20 shrink-0"
-          />
-          <Image
-            width={36}
-            height={53}
-            alt="Hero Vector 3"
-            src="/assets/icons/hero/Hero-vector3.svg"
-            className="absolute left-32 top-40 shrink-0"
-          />
-        </div>
-        <p className="absolute right-0 !my-0 w-[13.5rem] text-right text-base font-normal leading-normal text-[#002254]">
-          “You are <span className="font-bold">not alone</span> in your
-          <span className="font-bold"> struggles</span>.”
-        </p>
-        <p className=" !my-0 w-[17rem] text-base font-normal leading-normal text-[#002254]">
-          “There is <span className="font-bold">a crack</span> in everything,
-          that’s how <span className="font-bold">the light</span> gets in.”
-        </p>
-        <div className="absolute right-[4rem] top-44 flex w-[16.75rem] flex-col items-start justify-center gap-3 rounded-2xl bg-[#eaeaea4d] px-5 py-3 shadow-[0_8px_24px_0_rgba(0,0,0,0.08)] backdrop-blur-[25px]">
-          <div className=" flex items-center gap-2 self-stretch">
+          {StarIcons.map((each, index) => (
             <Image
-              onClick={() => console.log('clicked')}
+              key={index}
+              width={each.size}
+              height={each.size}
+              alt={`Hero star ${index}`}
+              src="/assets/images/icons/star-icon.svg"
+              className={mergeClassnames(
+                'absolute shrink-0',
+                each.xPosition && each.xPosition,
+                each.yPosition && each.yPosition,
+              )}
+            />
+          ))}
+          {VectorIcons.map((each, index) => (
+            <Image
+              key={index}
+              width={each.width}
+              height={each.height}
+              alt={`Hero vector ${index}`}
+              src={`/assets/images/icons/vectors/vector-${index}.svg`}
+              className={mergeClassnames(
+                'absolute shrink-0',
+                each.xPosition && each.xPosition,
+                each.yPosition && each.yPosition,
+              )}
+            />
+          ))}
+        </div>
+        <p className="absolute right-0 w-[13.5rem] text-right text-base font-normal leading-normal text-slate-1000">
+          {t.rich('hero_message_2', {
+            important: customMessage('font-bold'),
+          })}
+        </p>
+        <p className="w-[17rem] text-base font-normal leading-normal text-slate-1000">
+          {t.rich('hero_message_1', {
+            important: customMessage('font-bold'),
+          })}
+        </p>
+        <div
+          className={mergeClassnames(
+            'absolute right-16 top-44 flex flex-col items-start justify-center gap-3 rounded-2xl px-5 py-3',
+            ' w-[16.75rem] bg-[#eaeaea4d] shadow-[0_8px_24px_0_rgba(0,0,0,0.08)] backdrop-blur-[25px]',
+          )}
+        >
+          <div className="flex items-center gap-2 self-stretch">
+            <Image
+              // onClick={() => console.log('clicked')}
               width={56}
               height={56}
-              alt="Play-icon"
-              src="/assets/icons/Play-circle.svg"
+              alt="Play icon"
+              src="/assets/images/icons/play-circle.svg"
               className="cursor-pointer"
             />
             <Image
               width={164}
               height={46}
-              src="/assets/icons/Play-timeline.svg"
-              alt="Play-timeline"
+              src="/assets/images/soundwave.svg"
+              alt="Soundwave"
             />
           </div>
         </div>
         <div
-          id="shining"
-          className="absolute left-6 top-24 inline-flex h-20 w-[15.5rem] flex-col gap-3 overflow-hidden rounded-lg bg-[#ffffff4d] p-4 shadow-[0_8px_24px_0_rgba(0,0,0,0.08)] backdrop-blur-[20px]"
+          className={mergeClassnames(
+            'absolute left-6 top-24 inline-flex flex-col gap-3 overflow-hidden rounded-lg p-4',
+            'bg-[#ffffff4d] shadow-[0_8px_24px_0_rgba(0,0,0,0.08)] backdrop-blur-[20px]',
+          )}
         >
           <div className="relative flex flex-col items-start gap-1">
             <Image
               width={72}
               height={24}
-              src="/assets/icons/hero/Hero-mentors.svg"
-              alt="Hero-mentors"
+              src="/assets/images/mentors.svg"
+              alt="Mentor avatars"
             />
-            <p className="!my-0 text-base font-black leading-normal text-[#24272C]">
-              200+ <span className="font-normal">Experienced Mentors</span>
+            <p className="text-base font-black leading-normal text-slate-1000">
+              {t.rich('hero_mentor_quantity', {
+                normal: customMessage('font-normal'),
+              })}
             </p>
           </div>
         </div>
