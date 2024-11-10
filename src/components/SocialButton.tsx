0c1e093ce2db58d9156cf3eed4ec95c7cee20c94
silverian <dyrus1107@gmail.com>
2024-11-10 18:52:57 +0700

    feat(Login): added components and Login UI layout

--- src/components/SocialButton.tsx
+++ src/components/SocialButton.tsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import type { ReactNode } from 'react';
 import React from 'react';
 
 import IconButton from '@/components/iconButton/IconButton';
@@ -14,14 +15,25 @@ const SocialIcon = ({ iconUrl }: { iconUrl: string }) => (
   />
 );
 
-const SocialButton = ({ iconUrl }: { iconUrl: string }) => (
+const SocialButton = ({
+  iconUrl,
+  children,
+  className,
+}: {
+  iconUrl: string;
+  children?: ReactNode;
+  className?: string;
+}) => (
   <IconButton
     icon={<SocialIcon iconUrl={iconUrl} />}
     className={mergeClassnames(
-      'h-11 border border-solid border-[#E3E5EB] bg-white',
+      'h-11 border border-solid border-[#E3E5EB] bg-white text-center text-neutral-10 text-sm',
       'hover:border-[#E3E5EB] hover:bg-[#F3F4F6]',
+      className,
     )}
-  />
+  >
+    {children}
+  </IconButton>
 );
 
 export default SocialButton;
