a28903d327a80d9fafece64d9eebe0b8c498d0b7
hang nhung <hangbichnhung@gmail.com>
2025-01-02 18:01:56 +0700

    fix(HumanBookBanner): adjust layout and add class for image shrink behavior

--- src/layouts/HumanBookBanner.tsx
+++ src/layouts/HumanBookBanner.tsx
@@ -16,14 +16,14 @@ const HumanBookBanner = () => {
     <div className="relative mt-[18px] flex animate-flashing items-center justify-center bg-[linear-gradient(120deg,_#0442bf_30%,_#0858fa80_38%,_#0858fa80_40%,_#0442bf_48%)] bg-[length:200%_100%] bg-right-top px-12 sm:px-[1.875rem] lg:px-12 2xl:px-28">
       <div
         className={mergeClassnames(
-          'mx-auto flex w-full flex-col items-center justify-between px-3 py-8 sm:w-3/4 lg:max-w-7xl gap-6',
+          'mx-auto flex w-full flex-col items-center justify-between px-3 sm:w-3/4 lg:max-w-7xl gap-6',
           'lg:flex-row lg:gap-0',
         )}
       >
         <div className="flex flex-row items-center gap-3">
           <Link
             href={publicRoutes.LOGIN}
-            className="cursor-pointer whitespace-nowrap text-xl font-medium text-white hover:opacity-70"
+            className="cursor-pointer text-xl font-medium text-white hover:opacity-70"
           >
             {t('explore_now')}
           </Link>
@@ -34,6 +34,7 @@ const HumanBookBanner = () => {
           alt="banner"
           width={404}
           height={64}
+          className="shrink-0"
         />
         <div className="flex flex-row gap-4">
           <Link
