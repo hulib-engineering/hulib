e7e6acc71491107ff51911eba32a972c5b9fb6f0
Nhan Ngo <86797462+nhannt1311@users.noreply.github.com>
2025-01-04 21:25:42 +0700

    feat: create update personal information popup

--- src/components/confirmUpdatePopup/ConfirmUpdatePopup.tsx
+++ src/components/confirmUpdatePopup/ConfirmUpdatePopup.tsx
@@ -9,18 +9,22 @@ type Props = {
   open: boolean;
   onClose: () => void;
   onSuccess: () => void;
+  title: string;
+  description: string;
+  titleOfDiscardBtn: string;
+  titleOfConfirmBtn: string;
 };
-const ConfirmChangePassword = (props: Props) => {
+const ConfirmUpdatePopup = (props: Props) => {
   return (
     <Modal open={props.open} onClose={() => {}}>
       <Modal.Backdrop />
       <Modal.Panel className="!w-fit p-10">
         <div className="flex !w-fit flex-col items-center justify-center gap-5 rounded-lg bg-white">
           <h4 className="text-[28px] font-medium capitalize text-slate-1000">
-            Change Your Password
+            {props.title}
           </h4>
           <p className="text-base font-normal text-slate-1000">
-            Are you sure you want to change your password?
+            {props.description}
           </p>
           <div className="flex items-center gap-3">
             <Button
@@ -28,13 +32,13 @@ const ConfirmChangePassword = (props: Props) => {
               onClick={props.onClose}
               variant="outline"
             >
-              Cancel
+              {props.titleOfDiscardBtn}
             </Button>
             <Button
               className="rounded-full capitalize shadow-[0px_8px_24px_#1979ff40] transition-all duration-300 hover:shadow-none hover:translate-y-0.5"
               onClick={props.onSuccess}
             >
-              Change
+              {props.titleOfConfirmBtn}
             </Button>
           </div>
         </div>
@@ -43,4 +47,4 @@ const ConfirmChangePassword = (props: Props) => {
   );
 };
 
-export default ConfirmChangePassword;
+export default ConfirmUpdatePopup;
