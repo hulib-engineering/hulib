e0bac605b2d2e212d2828efce330b35c8eccb7a1
unknown <sonhoangdang0612@gmail.com>
2025-01-11 02:13:18 +0700

    [FE]: fix responsive register human book step

--- src/components/human-book-register/Success.tsx
+++ src/components/human-book-register/Success.tsx
@@ -16,7 +16,10 @@ const Success = () => {
     <div className="flex h-screen w-[384px] flex-col items-center justify-center gap-5">
       <CheckCircle className="size-[72px] text-[#46D51B]" />
       <p className="text-sm font-normal leading-5">
-        {t('success_submit_form')}
+        {t('success_submit_form_header')}
+      </p>
+      <p className="text-sm font-normal leading-5">
+        {t('success_submit_form_description')}
       </p>
       <Button className="mt-5 w-full" onClick={handleBackToHome}>
         {t('back_to_home')}
