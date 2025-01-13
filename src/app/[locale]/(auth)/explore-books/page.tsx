89c299975f6227fc11d38a63698d6329b879c6c4
unknown <sonhoangdang0612@gmail.com>
2025-01-13 22:40:11 +0700

    base

---
+++ src/app/[locale]/(auth)/explore-books/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Page = () => {
+  return (
+    <div className="flex w-full flex-col justify-between rounded-md bg-white px-4 py-8 md:px-28 md:py-12">
+      <h1 className="text-[2.5rem] font-bold text-neutral-20">Explore</h1>
+      <p className="text-lg font-normal text-neutral-20">
+        Discover and find your perfect book
+      </p>
+    </div>
+  );
+};
+
+export default Page;
