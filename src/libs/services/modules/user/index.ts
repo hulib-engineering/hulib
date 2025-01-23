f16da74f5e944b4de08ee7a53e1ff5f57da71b8c
hang nhung <hangbichnhung@gmail.com>
2025-01-23 19:43:15 +0700

    feat: add create story form and user fetching API

--- src/libs/services/modules/user/index.ts
+++ src/libs/services/modules/user/index.ts
@@ -1,15 +1,18 @@
 import { api } from '@/libs/services/api';
 import getAuthorDetail from '@/libs/services/modules/user/getAuthorDetail';
 
+import getUsers from './getUsers';
+
 const authenticationApiWithTag = api.enhanceEndpoints?.({
-  addTagTypes: ['User'],
+  addTagTypes: ['Users'],
 });
 
 export const userApi = authenticationApiWithTag.injectEndpoints({
   endpoints: (build: any) => ({
+    getUsers: getUsers(build),
     getAuthorDetail: getAuthorDetail(build),
   }),
   overrideExisting: false,
 });
 
-export const { useGetAuthorDetailQuery }: any = userApi;
+export const { useGetAuthorDetailQuery, useGetUsersQuery }: any = userApi;
