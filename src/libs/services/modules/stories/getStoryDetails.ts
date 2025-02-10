9e664722bf2c51b949275b36f6809c1755d9ff70
hang nhung <hangbichnhung@gmail.com>
2025-02-10 23:54:58 +0700

    chore(stories): simplify getStoryDetails query and serialization

--- src/libs/services/modules/stories/getStoryDetails.ts
+++ src/libs/services/modules/stories/getStoryDetails.ts
@@ -1,22 +1,15 @@
 import type { BaseQueryFn } from '@reduxjs/toolkit/query';
 import type { EndpointBuilder } from '@reduxjs/toolkit/src/query/endpointDefinitions';
 
-import type { PaginatedResponse } from '../../type';
 import type { Story, StoryDetailsParams } from './storiesType';
 
 const getStoryDetails = (build: EndpointBuilder<BaseQueryFn, string, string>) =>
   build.query<Story, StoryDetailsParams>({
-    query: (params) => ({
-      url: `stories`,
-      params: {
-        id: params?.id || 0,
-      },
+    query: ({ id }) => ({
+      url: `stories/${id || 0}`,
     }),
-    transformResponse: (response: PaginatedResponse<Story>) => {
-      return response.data[0] || ({} as Story);
-    },
-    serializeQueryArgs: (params) => {
-      return params;
+    serializeQueryArgs: ({ endpointName, queryArgs }) => {
+      return `${endpointName}-${queryArgs.id || 0}`;
     },
     providesTags: (result, error, { id }) => [
       { type: 'Story' as const, id: id || 0 },
