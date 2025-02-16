66101e525357612e20177946da3b16ade4e9a81b
Truong Ma Phi <trmaphi@gmail.com>
2025-02-16 13:12:53 +0700

    chore: update to avoid plan limit

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -10,16 +10,16 @@ Sentry.init({
   dsn: 'https://16bc7f4e4d00a7cc86ec916d6b625518@o4506568698626048.ingest.sentry.io/4506568729100288',
 
   // Adjust this value in production, or use tracesSampler for greater control
-  tracesSampleRate: 1,
+  tracesSampleRate: 0.5,
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
 
-  replaysOnErrorSampleRate: 1.0,
+  replaysOnErrorSampleRate: 0,
 
   // This sets the sample rate to be 10%. You may want this to be 100% while
   // in development and sample at a lower rate in production
-  replaysSessionSampleRate: 0.1,
+  replaysSessionSampleRate: 0,
 
   // You can remove this option if you're not planning to use the Sentry Session Replay feature:
   integrations: [
