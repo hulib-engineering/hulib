80a701e1e58f2804dac93ff6e3391a38490710c4
Nhan Ngo <86797462+nhannt1311@users.noreply.github.com>
2025-02-18 21:19:19 +0700

    fix: view more button in explore story not working

--- src/libs/services/modules/stories/getStories.ts
+++ src/libs/services/modules/stories/getStories.ts
@@ -10,29 +10,15 @@ const getStories = (build: EndpointBuilder<BaseQueryFn, string, string>) =>
       url: 'stories',
       params: {
         page: params?.page || 1,
-        limit: params?.limit || 10,
+        limit: params?.limit,
         topicIds: params?.topicIds,
       },
     }),
     serializeQueryArgs: ({ endpointName, queryArgs }) => {
       return `${endpointName}(${JSON.stringify(queryArgs?.topicIds)})`;
     },
-    merge: (currentCache, newItems, { arg }) => {
-      if (arg?.page === 1) {
-        return newItems; // Nếu topicIds thay đổi hoặc là trang đầu tiên, làm mới toàn bộ
-      }
-      return {
-        ...newItems,
-        data: [...currentCache.data, ...newItems.data],
-      };
-    },
     forceRefetch: ({ currentArg, previousArg }) => {
-      // Trigger lại fetch khi topicIds hoặc page thay đổi
-      return (
-        currentArg?.page !== previousArg?.page ||
-        JSON.stringify(currentArg?.topicIds) !==
-          JSON.stringify(previousArg?.topicIds)
-      );
+      return currentArg?.limit !== previousArg?.limit;
     },
     providesTags: (result) =>
       result
