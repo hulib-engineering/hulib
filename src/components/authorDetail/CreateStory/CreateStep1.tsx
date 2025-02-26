fe2ef85b17a3d627f216039aaec53fe4ce7e4007
Nhan Ngo <86797462+nhannt1311@users.noreply.github.com>
2025-02-26 23:39:28 +0700

    chore: update height of text area abstract

--- src/components/authorDetail/CreateStory/CreateStep1.tsx
+++ src/components/authorDetail/CreateStory/CreateStep1.tsx
@@ -1,11 +1,12 @@
 'use client';
 
+import { Trash } from '@phosphor-icons/react';
+import Image from 'next/image';
 import * as React from 'react';
 import type { UseFormReturn } from 'react-hook-form';
 import { useController } from 'react-hook-form';
 
 import { MultipleInputValue } from '@/components/authorDetail/MultipleInputValue';
-import { UploadFileData } from '@/components/authorDetail/UploadFileData';
 import Button from '@/components/button/Button';
 import Form from '@/components/form/Form';
 import TextInput from '@/components/textInput/TextInput';
@@ -45,12 +46,32 @@ export const CreateStep1 = (props: Props) => {
     name: 'uploadFile',
   });
 
+  const elmnt = document.getElementById('leftPanel');
+  const panelHeight = elmnt?.clientHeight ?? 440;
+  const rightPanelWidth = elmnt?.clientWidth ?? 400;
+
+  const [selectedImage, setSelectedImage] = React.useState<string>();
+  const [height, setHeight] = React.useState<number>(panelHeight - 20);
+
   const onChangeTopics = (topicsList: string[]) => {
     topicsField.onChange([...topicsList]);
   };
 
   const onUploadFile = (file: File | undefined) => {
     uploadFileField.onChange(file);
+  };
+
+  const uploadFile = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    const file = e.target.files?.[0];
+    setSelectedImage(file ? URL.createObjectURL(file) : undefined);
+    onUploadFile(file);
+    setHeight(height + 400);
+  };
+
+  const removeImage = (): void => {
+    setSelectedImage(undefined);
+    onUploadFile(undefined);
+    setHeight(height - 400);
   };
 
   const onHandleSubmit = handleSubmit((data) => {
@@ -60,11 +81,28 @@ export const CreateStep1 = (props: Props) => {
     }
   });
 
+  const renderAbstract = React.useMemo(() => {
+    return (
+      <textarea
+        id="abstract"
+        placeholder="Hey, let's talk about your inspire story"
+        style={{
+          height: `${height}px`,
+        }}
+        className="mt-1 w-full resize-none rounded-lg border border-neutral-90 bg-neutral-98 p-2 text-xs"
+        {...register('abstract')}
+      />
+    );
+  }, [height]);
+
   return (
     <Form id="next-step" onSubmit={onHandleSubmit}>
       <div className="flex flex-col gap-y-8">
         <div className="mdDown:gap-4 xsDown:grid-cols-1 relative grid w-full grid-cols-10 gap-6">
-          <div className="xsDown:w-full xsDown:col-span-1 col-span-4 flex h-full flex-col gap-y-2">
+          <div
+            id="leftPanel"
+            className="xsDown:w-full xsDown:col-span-1 col-span-4 flex h-fit flex-col gap-y-2"
+          >
             <div className="flex-1 flex-col gap-y-2">
               <p className="text-sm text-neutral-10">
                 Topics<span className="text-red-50">*</span>
@@ -104,23 +142,72 @@ export const CreateStep1 = (props: Props) => {
               <p className="text-sm text-neutral-10">
                 Cover picture<span className="text-red-50">*</span>
               </p>
-              <div className="mt-4">
-                <UploadFileData uploadSelectedFile={onUploadFile} />
+              <div className="mt-4 w-full">
+                <div className="flex flex-col gap-y-4">
+                  {selectedImage && (
+                    <div className="relative">
+                      <Image
+                        src={selectedImage}
+                        alt="Selected Image"
+                        width={rightPanelWidth}
+                        height={400}
+                        className="h-[400px] rounded-lg border border-neutral-80 text-xs"
+                      />
+                      <button
+                        type="button"
+                        className="absolute -right-2 -top-2 z-auto cursor-pointer rounded-full bg-primary-60 p-2 hover:bg-primary-40"
+                        onClick={removeImage}
+                      >
+                        <Trash size={16} color="white" />
+                      </button>
+                    </div>
+                  )}
+
+                  <div className="flex flex-col gap-y-4">
+                    <div className="flex flex-col items-center">
+                      <input
+                        id="upload"
+                        type="file"
+                        accept="image/*"
+                        className="hidden"
+                        multiple={false}
+                        onChange={uploadFile}
+                        value=""
+                      />
+                      {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+                      <label
+                        htmlFor="upload"
+                        className="flex w-full cursor-pointer items-center justify-center gap-y-1 rounded-full border border-neutral-80 py-2 text-base font-medium text-primary-50"
+                      >
+                        Upload Picture (under 50 MB)
+                      </label>
+                    </div>
+                    <div className="flex flex-col">
+                      <input
+                        id="upload"
+                        type="file"
+                        accept="image/*"
+                        className="hidden"
+                        multiple={false}
+                      />
+                      {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+                      <label
+                        htmlFor="upload"
+                        className="flex w-full cursor-pointer items-center justify-center gap-y-1 rounded-full border border-neutral-80 py-2 text-base font-medium text-primary-50"
+                      >
+                        Choose System Design
+                      </label>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
           <div className="xsDown:w-full xsDown:col-span-1 col-span-6 flex h-full flex-col">
-            <p className="text-sm text-neutral-10">
+            <p className="h-4 text-sm text-neutral-10">
               Your story abstract<span className="text-red-50">*</span>
             </p>
-            <Form.Item>
-              <textarea
-                id="abstract"
-                placeholder="Hey, let's talk about your inspire story"
-                className="min-h-full w-full resize-none rounded-lg border border-neutral-90 bg-neutral-98 p-2 text-xs"
-                {...register('abstract')}
-              />
-            </Form.Item>
+            <Form.Item>{renderAbstract}</Form.Item>
           </div>
         </div>
       </div>
