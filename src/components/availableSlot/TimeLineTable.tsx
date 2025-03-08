1ba46aaed8167f5dfd1ff943de8afde1703130ce
Nhan Ngo <86797462+nhannt1311@users.noreply.github.com>
2025-03-08 16:44:04 +0700

    chore: update ui/ux available slot

--- src/components/availableSlot/TimeLineTable.tsx
+++ src/components/availableSlot/TimeLineTable.tsx
@@ -2,8 +2,13 @@ import * as React from 'react';
 
 import { mergeClassnames } from '@/components/private/utils';
 
-export const TimeLineTable = () => {
-  const [selectedTime, setSelectedTime] = React.useState<string[]>([]);
+type Props = {
+  initialSelectedTime: string[];
+  onChange: (value: string[]) => void;
+};
+export const TimeLineTable = ({ onChange, initialSelectedTime }: Props) => {
+  const [selectedTime, setSelectedTime] =
+    React.useState<string[]>(initialSelectedTime);
   const items = [];
   for (let hour = 6; hour < 24; hour += 1) {
     items.push([hour, 0]);
@@ -29,10 +34,12 @@ export const TimeLineTable = () => {
     if (selectedTime?.includes(time)) {
       const newSelectedTime = selectedTime.filter((item) => item !== time);
       setSelectedTime(newSelectedTime);
+      onChange(newSelectedTime);
       return;
     }
     const newVal: string[] = [...selectedTime, time];
     setSelectedTime(newVal);
+    onChange(newVal);
   };
 
   const timeItem = (value: string) => {
@@ -57,7 +64,7 @@ export const TimeLineTable = () => {
       return null;
     }
     return (
-      <div className="grid w-full grid-cols-5 items-center gap-2 rounded-lg bg-[#F9F9F9] p-3 md:grid-cols-6">
+      <div className="md:grid-cols-6 grid w-full grid-cols-5 items-center gap-2 rounded-lg bg-[#F9F9F9] p-3">
         {list.map((item) => (
           <>{timeItem(item)}</>
         ))}
