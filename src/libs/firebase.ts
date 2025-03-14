b0671562c0f4942df388380b52c53cd2cc26d79d
VuHoaBinh <hoabinh.vippro63@gmail.com>
2025-03-14 22:01:06 +0700

    fix: fix bug verfired recaptcha

--- src/libs/firebase.ts
+++ src/libs/firebase.ts
@@ -1,17 +1,36 @@
-import { getApps, initializeApp } from 'firebase/app';
+// import { getApps, initializeApp } from 'firebase/app';
+// import { getAuth } from 'firebase/auth';
+
+// const firebaseConfig = {
+//   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+//   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+//   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+//   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+//   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+//   measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+
+// };
+
+// export const firebaseApp =
+//   getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
+
+// export const auth = getAuth(firebaseApp);
+
+// console.log("✅ Firebase initialized:", firebaseApp);
+// console.log("✅ Firebase Auth:", auth);
+
+import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 
-export const firebaseConfig = {
-  apiKey: 'AIzaSyAYqF2tLG1bMoNzBMLtOPfoB2Fp_vTrdLU',
-  authDomain: 'hulib-9a3ce.firebaseapp.com',
-  projectId: 'hulib-9a3ce',
-  storageBucket: 'hulib-9a3ce.firebasestorage.app',
-  messagingSenderId: '637775863926',
-  appId: '1:637775863926:web:bb2fc4dbfc93780fdf7f57',
-  measurementId: 'G-FP85CZ4YKP',
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
-export const firebaseApp =
-  getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
-
-export const auth = getAuth(firebaseApp);
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
