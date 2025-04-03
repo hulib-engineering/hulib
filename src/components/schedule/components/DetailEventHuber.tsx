994af9dc5f4fe82ee1cb31fb4060c8ebe154fc1a
Long Tran <tranxuanlonga555@gmail.com>
2025-04-03 20:21:34 +0700

    fix: eslint

--- src/components/schedule/components/DetailEventHuber.tsx
+++ src/components/schedule/components/DetailEventHuber.tsx
@@ -3,12 +3,11 @@
 import { CaretUp, Clock, Note, User } from '@phosphor-icons/react';
 import Image from 'next/image';
 
-function DetailEventHuber({ data,callblack }: any) {
-  console.log('Data', data);
-  const { startedAt, endedAt, reader, humanBook,id, story } = data;
+function DetailEventHuber({ data, callblack }: any) {
+  const { startedAt, endedAt, reader, humanBook, id, story } = data;
   const { fullName: fullNameReader } = reader;
   const { fullName: fullNameHumanBook } = humanBook;
-  const {abstract} = story;
+  const { abstract } = story;
   function formatDateTimeRange(startIso: string, endIso: string) {
     const startDate = new Date(startIso);
     const endDate = new Date(endIso);
@@ -31,34 +30,34 @@ function DetailEventHuber({ data,callblack }: any) {
     return `${dayOfWeek}, ${day}/${month}/${year} | ${startHours}:${startMinutes} - ${endHours}:${endMinutes}`;
   }
 
-  const changeStatus = async (status : string) => {
+  const changeStatus = async (status: string) => {
     fetch(`https://hulib-services.onrender.com/api/v1/reading-sessions/${id}`, {
       method: 'PATCH',
       headers: {
-          'Content-Type': 'application/json'
+        'Content-Type': 'application/json',
       },
       body: JSON.stringify({
         sessionStatus: status,
+      }),
+    })
+      .then((response) => response.json())
+      .then((res) => {
+        console.log('res', res);
+        callblack(true);
       })
-
-  })
-  .then(response => response.json())
-  .then(data => {
-    console.log("data",data)
-    callblack(true)})
-  .catch(error => console.error('Error:', error));
-  }
+      .catch((error) => console.error('Error:', error));
+  };
   return (
     <div className="h-max w-[396px] overflow-hidden rounded-[16px] bg-[#F0F5FF] px-[20px] py-[16px] shadow-[#1C1E211A] drop-shadow-sm">
       <div className="flex items-center">
         <h3 className="flex-1 text-[20px] leading-[32px] text-[#171819]">
           Session with Liber
         </h3>
-        {data.sessionStatus === "pending" ? (
-         <p className="mx-[8px] h-[32px] w-[107px] rounded-[100px] bg-[#FFC745] px-[10px] py-[2px] text-center align-middle text-[#000000]">
-         {data.sessionStatus === "pending" ? "Pending..." : ""}
-        </p>)
-         : null}     
+        {data.sessionStatus === 'pending' ? (
+          <p className="mx-[8px] h-[32px] w-[107px] rounded-[100px] bg-[#FFC745] px-[10px] py-[2px] text-center align-middle text-[#000000]">
+            {data.sessionStatus === 'pending' ? 'Pending...' : ''}
+          </p>
+        ) : null}
         <CaretUp size={24} color="#000" className="-mt-[4px]" />
       </div>
       <div className="mt-[16px] flex items-center">
@@ -111,26 +110,28 @@ function DetailEventHuber({ data,callblack }: any) {
       </div>
       <div className="mt-[16px] flex items-start">
         <Note size={40} color="#000" />
-        <p className="ml-[8px] text-[14px] font-[400] leading-[20px] text-[#2E3032] max-h-[20px] overflow-hidden text-ellipsis">
+        <p className="ml-[8px] max-h-[20px] overflow-hidden text-ellipsis text-[14px] font-[400] leading-[20px] text-[#2E3032]">
           {abstract}
         </p>
       </div>
-      {data.sessionStatus === "pending" && (<div className="mt-[16px] flex items-center">
-        <button
-          type="button"
-          onClick={() => changeStatus("canceled")}
-          className="h-[44px] w-[141px] rounded-full border border-[#C2C6CF] bg-[#fff] text-[16px] font-[500] leading-[20px] text-[#0442BF]"
-        >
-          Reject
-        </button>
-        <button
-          type="button"
-          onClick={() => changeStatus("approved")}
-          className="ml-auto h-[44px] min-w-[205px] rounded-full bg-[#0442BF] text-[16px] font-[500] leading-[20px] text-[#fff]"
-        >
-          Approve
-        </button>
-      </div>)}
+      {data.sessionStatus === 'pending' && (
+        <div className="mt-[16px] flex items-center">
+          <button
+            type="button"
+            onClick={() => changeStatus('canceled')}
+            className="h-[44px] w-[141px] rounded-full border border-[#C2C6CF] bg-[#fff] text-[16px] font-[500] leading-[20px] text-[#0442BF]"
+          >
+            Reject
+          </button>
+          <button
+            type="button"
+            onClick={() => changeStatus('approved')}
+            className="ml-auto h-[44px] min-w-[205px] rounded-full bg-[#0442BF] text-[16px] font-[500] leading-[20px] text-[#fff]"
+          >
+            Approve
+          </button>
+        </div>
+      )}
     </div>
   );
 }
