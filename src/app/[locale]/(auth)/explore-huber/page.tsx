0a82ca4996ac961876d404e836ff649c28cb0705
hang nhung <hangbichnhung@gmail.com>
2025-04-07 22:02:10 +0700

    fix(layout): fix common layout

--- src/app/[locale]/(auth)/explore-huber/page.tsx
+++ src/app/[locale]/(auth)/explore-huber/page.tsx
@@ -1,5 +1,11 @@
 const ExploreHuber = () => {
-  return <div>ExploreHuber</div>;
+  return (
+    <div className="px-28 py-12">
+      <div className="mx-auto h-full w-full max-w-[1216px] rounded-lg">
+        Hubers
+      </div>
+    </div>
+  );
 };
 
 export default ExploreHuber;
