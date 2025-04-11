13aa377625ded7f103093ae0cace78723b9be7df
hang nhung <hangbichnhung@gmail.com>
2025-04-11 22:45:52 +0700

    fix(huber-registration-step1): update UI and bind api

--- src/validations/StoriesValidation.ts
+++ src/validations/StoriesValidation.ts
@@ -4,7 +4,4 @@ export const StoriesValidation = z.object({
   title: z.string().min(1, 'Title is required'),
   abstract: z.string().min(1, 'Abstract is required'),
   topics: z.array(z.number()).min(1, 'Please select at least one topic'),
-  cover: z.object({
-    id: z.string().min(1, 'Cover is required'),
-  }),
 });
