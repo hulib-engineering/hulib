f0dca5a66eee8b054af50266140c18dc6ad38c01
TuCongDanh12 <tucondanh9a1@gmail.com>
2025-04-19 22:32:00 +0700

    fix: api huber

--- src/components/huber/ListHuber.tsx
+++ src/components/huber/ListHuber.tsx
@@ -2,7 +2,7 @@
 
 import { CaretCircleDown, CaretCircleUp } from '@phosphor-icons/react';
 import { useTranslations } from 'next-intl';
-import React, { useEffect } from 'react';
+import React from 'react';
 
 import Button from '@/components/button/Button';
 import StoriesSkeleton from '@/components/stories/StoriesSkeleton';
@@ -13,7 +13,7 @@ import type { Topic } from '@/libs/services/modules/topics/topicType';
 import Huber from './Huber';
 
 type ListHuberProps = {
-  topicId: string | null;
+  topicId: String[] | null;
   topics: Topic[];
 };
 
@@ -21,17 +21,15 @@ const ListHuber = ({ topicId, topics }: ListHuberProps) => {
   const [limit, setLimit] = React.useState(6);
   const [isExpandList, setIsExpandList] = React.useState(false);
   const t = useTranslations('ExporeStory');
+
   const {
-    data: hubersPages,
+    data: hubersData,
     isLoading,
     isFetching,
   } = useGetHubersQuery({
     page: 1,
     limit,
     topicId: topicId || undefined,
-  });
-  useEffect(() => {
-    console.log('hubersPages', hubersPages);
   });
 
   const onClickSeeAll = () => {
@@ -40,35 +38,33 @@ const ListHuber = ({ topicId, topics }: ListHuberProps) => {
 
   React.useEffect(() => {
     if (isExpandList) {
-      setLimit(0);
+      setLimit(hubersData?.meta?.totalItems || 100);
     } else {
       setLimit(6);
     }
-  }, [isExpandList]);
+  }, [isExpandList, hubersData?.meta?.totalItems]);
 
   if (isLoading || isFetching) return <StoriesSkeleton />;
+
+  const hasMoreData =
+    hubersData?.meta?.currentPage < hubersData?.meta?.totalPages;
 
   return (
     <div className="w-full py-6">
       <div className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-4">
-        {/* {hubers.map((huber) => (
-          <div key={huber.id} className="w-full">
-            <Huber />
-          </div>
-        ))} */}
-        {hubersPages?.data?.map((huber: HuberType) => (
+        {hubersData?.data?.map((huber: HuberType) => (
           <div key={huber.id} className="w-full">
             <Huber key={huber.id} data={huber} topics={topics} />
           </div>
         ))}
       </div>
-      {(limit === 6 && hubersPages?.hasNextPage) ||
-      (limit === 0 && !hubersPages.hasNextPage) ? (
+      {(limit === 6 && hasMoreData) ||
+      (limit > 6 && hubersData?.data.length > 6) ? (
         <div className="mt-6 flex w-full items-center justify-center">
           <Button
             variant="outline"
             iconLeft={
-              limit === 0 ? (
+              isExpandList ? (
                 <CaretCircleUp size={16} color="#0442BF" />
               ) : (
                 <CaretCircleDown size={16} color="#0442BF" />
@@ -76,7 +72,7 @@ const ListHuber = ({ topicId, topics }: ListHuberProps) => {
             }
             onClick={onClickSeeAll}
           >
-            {limit === 0 ? t('hide_all') : t('view_more')}
+            {isExpandList ? t('hide_all') : t('view_more')}
           </Button>
         </div>
       ) : null}
