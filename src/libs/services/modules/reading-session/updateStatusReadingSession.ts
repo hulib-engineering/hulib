cd083a07aee7335e6043de19a334d981ecce6e18
hang nhung <hangbichnhung@gmail.com>
2025-04-23 23:52:28 +0700

    feat: integrate user details in meeting scheduling and enhance reading session management

---
+++ src/libs/services/modules/reading-session/updateStatusReadingSession.ts
@@ -0,0 +1,26 @@
+import type { BaseQueryFn, EndpointBuilder } from '@reduxjs/toolkit/query';
+
+import type { PaginatedResponse } from '../../type';
+import type { ReadingSession } from './createNewReadingSession';
+
+const updateStatusReadingSession = (
+  build: EndpointBuilder<BaseQueryFn, string, string>,
+) =>
+  build.mutation<
+    PaginatedResponse<ReadingSession>,
+    {
+      id: number;
+      sessionStatus: string;
+    }
+  >({
+    query: ({ id, sessionStatus }) => ({
+      url: `reading-sessions/${id}`,
+      method: 'PATCH',
+      body: JSON.stringify({
+        sessionStatus,
+      }),
+    }),
+    invalidatesTags: [{ type: 'ReadingSession' }],
+  });
+
+export default updateStatusReadingSession;
