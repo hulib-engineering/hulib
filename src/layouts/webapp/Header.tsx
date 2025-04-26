bda6fcc28d995ab628ad5509f664326b5427e320
TuCongDanh12 <tucondanh9a1@gmail.com>
2025-04-26 10:18:22 +0700

    chore: add switcher

--- src/layouts/webapp/Header.tsx
+++ src/layouts/webapp/Header.tsx
@@ -16,6 +16,7 @@ import React, { useMemo } from 'react';
 
 import Button from '@/components/button/Button';
 import IconButton from '@/components/iconButton/IconButton';
+import { LocaleSwitcher } from '@/components/LocaleSwitcher';
 import { Logo } from '@/components/Logo';
 import MenuItem from '@/components/menuItem/MenuItem';
 import Popover from '@/components/popover/Popover';
@@ -117,7 +118,7 @@ const AvatarPopoverContent: React.FC<RenderProps> = ({
 };
 
 const AvatarPopover = ({ children }: WithChildren<{}>) => (
-  <Popover position="bottom-end" className="h-full">
+  <Popover position="bottom-end" className="h-full w-11">
     <Popover.Trigger
       data-testid="popover-trigger-arrow"
       {...{
@@ -210,6 +211,7 @@ const Header = () => {
                   <CaretDown size={12} />
                 </div>
               </div>
+              <LocaleSwitcher className="shrink" />
             </div>
           )}
         </div>
@@ -269,6 +271,7 @@ const Header = () => {
                 <CaretDown size={12} />
               </div>
             </div>
+            <LocaleSwitcher className="shrink" />
           </div>
         )}
       </header>
