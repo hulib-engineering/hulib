9cc444b41f5eb63588a6915d503e82957701ccdd
HangNhung <hangbichnhung@gmail.com>
2025-04-30 09:49:55 +0700

    feat: refactor CustomCoverBook component and integrate it into Step3 and FlipBook components

--- src/validations/StoriesValidation.ts
+++ src/validations/StoriesValidation.ts
@@ -4,8 +4,8 @@ export const StoriesValidation = z.object({
   title: z.string().min(1, 'Title is required'),
   abstract: z.string().min(1, 'Abstract is required'),
   topicIds: z.array(z.number()).min(1, 'Please select at least one topic'),
-  // cover: z.object({
-  // 	id: z.string().min(1, 'Cover is required'),
-  // 	path: z.string().min(1, 'Cover is required'),
-  // }),
+  cover: z.object({
+    id: z.string().min(1, 'Cover is required'),
+    path: z.string().min(1, 'Cover is required'),
+  }),
 });
