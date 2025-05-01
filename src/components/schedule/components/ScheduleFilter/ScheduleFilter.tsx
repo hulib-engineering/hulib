4739f5bfece49695c24edb29f78c6f66e0aacba1
nhan-ngo <86797462+nhannt1311@users.noreply.github.com>
2025-05-01 20:48:08 +0700

    feat: update ui

--- src/components/schedule/components/ScheduleFilter/ScheduleFilter.tsx
+++ src/components/schedule/components/ScheduleFilter/ScheduleFilter.tsx
@@ -27,10 +27,15 @@ const ScheduleFilterPopoverMenuItems = [
 
 const ScheduleFilterPopoverContent: React.FC<RenderProps> = () => {
   return (
-    <div data-testid="popover-content" className="grid grid-cols-2">
+    <div data-testid="popover-content" className="grid grid-cols-2 gap-y-4">
       {ScheduleFilterPopoverMenuItems.map((item, index) => (
-        <div key={index} className="flex items-center gap-x-1">
-          <Input type="checkbox" id={item.label} readOnly={false} />
+        <div key={index} className="flex h-5 flex-1 items-center gap-x-1.5">
+          <Input
+            type="checkbox"
+            id={item.label}
+            readOnly={false}
+            className="flex h-3 w-3 flex-1"
+          />
           <Label
             htmlFor={item.label}
             type="checkbox"
@@ -54,10 +59,10 @@ export const ScheduleFilterPopover = () => (
     >
       <div className="flex items-center gap-x-2 rounded-lg border border-[#ABAEB1] px-4 py-1 text-sm text-neutral-40">
         Type of meeting
-        <CaretDown size={12} />
+        <CaretDown size={16} />
       </div>
     </Popover.Trigger>
-    <Popover.Panel className="flex flex-col gap-1 p-2">
+    <Popover.Panel className="flex h-fit w-[165px] flex-col">
       {({ open = false, close }) => (
         <ScheduleFilterPopoverContent close={close} open={open} />
       )}
