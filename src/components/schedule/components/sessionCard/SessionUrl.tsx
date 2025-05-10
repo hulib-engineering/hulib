af4053762fd7b2ed7764f49b9b9985302d91e3b3
TuCongDanh12 <tucondanh9a1@gmail.com>
2025-05-10 21:52:14 +0700

    feat: meeting component

--- src/components/schedule/components/sessionCard/SessionUrl.tsx
+++ src/components/schedule/components/sessionCard/SessionUrl.tsx
@@ -1,4 +1,4 @@
-import { LinkSimple } from '@phosphor-icons/react';
+import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 
@@ -8,12 +8,18 @@ interface SessionUrlProps {
 
 export const SessionUrl: React.FC<SessionUrlProps> = ({ url }) => {
   return (
-    <div className="mb-3 flex items-center text-sm">
-      <LinkSimple size={16} className="mr-2 text-blue-500" />
+    <div className="mb-3 flex items-center gap-1 text-sm">
+      <Image
+        src="/assets/icons/MapPinArea.svg"
+        alt="link meet"
+        width={16}
+        height={16}
+        className="shrink-0"
+      />
       <Link
         href={url}
         passHref
-        className="truncate text-blue-500"
+        className="truncate text-blue-500 underline"
         target="_blank"
         rel="noopener noreferrer"
       >
