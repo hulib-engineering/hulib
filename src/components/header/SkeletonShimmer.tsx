91e16b31df5aed67e13d30cd85f82edcd3911128
quynhdeptrai306 <nguyentrucquynh3006@gmail.com>
2025-05-11 17:27:28 +0700

    fix: custom Shimmer

--- src/components/header/SkeletonShimmer.tsx
+++ src/components/header/SkeletonShimmer.tsx
@@ -2,7 +2,7 @@
 export default function SkeletonShimmer() {
   return (
     <div className="relative h-[40px] w-[200px] overflow-hidden rounded-full bg-gray-300">
-      <div className="animate-shimmer absolute left-[-150px] top-0 h-full w-[150px] bg-gradient-to-r from-transparent via-white/60 to-transparent" />
+      <div className="absolute left-[-150px] top-0 h-full w-[150px] animate-shimmer bg-gradient-to-r from-transparent via-white/60 to-transparent" />
     </div>
   );
 }
