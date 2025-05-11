79a590dabe84815b42760c719fd6239052e48c0e
quynhdeptrai306 <nguyentrucquynh3006@gmail.com>
2025-05-11 17:18:48 +0700

    fix: updated Skeleton because test is Erorr

--- src/components/header/SkeletonShimmer.tsx
+++ src/components/header/SkeletonShimmer.tsx
@@ -1,9 +1,8 @@
-import './SkeletonShimmer.css';
-
+/* eslint-disable tailwindcss/no-custom-classname */
 export default function SkeletonShimmer() {
   return (
-    <div className="skeleton-wrapper">
-      <div className="skeleton" />
+    <div className="relative h-[40px] w-[200px] overflow-hidden rounded-full bg-gray-300">
+      <div className="animate-shimmer absolute left-[-150px] top-0 h-full w-[150px] bg-gradient-to-r from-transparent via-white/60 to-transparent" />
     </div>
   );
 }
