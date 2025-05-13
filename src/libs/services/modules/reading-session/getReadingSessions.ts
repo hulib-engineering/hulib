6aaa3ecbd67cb91507dd4a682be20227b5f17430
Hang Nhung <hangbichnhung@gmail.com>
2025-05-13 22:51:51 +0700

    feat(schedule): enhance BigSchedule component with SessionCard and improve reading session query

--- src/libs/services/modules/reading-session/getReadingSessions.ts
+++ src/libs/services/modules/reading-session/getReadingSessions.ts
@@ -4,11 +4,22 @@ import type { EndpointBuilder } from '@reduxjs/toolkit/src/query/endpointDefinit
 const getReadingSessions = (
   build: EndpointBuilder<BaseQueryFn, string, string>,
 ) =>
-  build.query<any, { upcoming?: boolean }>({
-    query: (params) =>
-      `reading-sessions?${
-        params?.upcoming ? `upcoming=${params?.upcoming}` : ''
-      }`,
+  build.query<any, { upcoming?: boolean; page?: number; limit?: number }>({
+    query: (params) => {
+      // Set default values for pagination
+      const page = params?.page ?? 1;
+      const limit = params?.limit ?? 1000;
+
+      const queryString = new URLSearchParams();
+      if (params?.upcoming)
+        queryString.append('upcoming', params.upcoming.toString());
+
+      // Always add page and limit with default values
+      queryString.append('page', page.toString());
+      queryString.append('limit', limit.toString());
+
+      return `reading-sessions?${queryString.toString()}`;
+    },
     providesTags: [{ type: 'ReadingSession' }],
   });
 
