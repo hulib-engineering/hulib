7a033971147ec9b6230597961bee56c5966c4076
Quoc Cuong <cuongnhq@nal.vn>
2025-05-29 19:42:54 +0700

    Get channel and token from url

--- src/app/[locale]/(auth)/reading/page.tsx
+++ src/app/[locale]/(auth)/reading/page.tsx
@@ -1,9 +1,12 @@
 import dynamic from 'next/dynamic';
 
 // Dynamic import để dùng component client
-const AgoraVideoCall = dynamic(() => import('./components/AgoraVideoCall'), {
-  ssr: false,
-});
+const AgoraVideoCall = dynamic(
+  () => import('../../../../components/meeting/AgoraVideoCall'),
+  {
+    ssr: false,
+  }
+);
 
 export default function ReadingPage() {
   const appId = process.env.NEXT_PUBLIC_AGORA_APP_ID || '';
