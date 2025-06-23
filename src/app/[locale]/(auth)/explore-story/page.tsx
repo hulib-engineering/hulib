a029a26f47cad0a81db5f78b4f5172d779d3b1e9
HangNhung <hangbichnhung@gmail.com>
2025-06-23 19:42:00 +0700

    refactor(ui): enhance layout and default image handling in ExploreStory components

--- src/app/[locale]/(auth)/explore-story/page.tsx
+++ src/app/[locale]/(auth)/explore-story/page.tsx
@@ -8,5 +8,9 @@ export default function Index() {
   const searchParams = useSearchParams();
   const topicIds = searchParams.get('topicIds'); // Get topicIds from the URL query string
 
-  return <ExploreStory topicIds={topicIds} />;
+  return (
+    <div className="mx-auto w-full max-w-screen-sm px-4 lg:max-w-screen-2xl lg:px-28">
+      <ExploreStory topicIds={topicIds} />
+    </div>
+  );
 }
