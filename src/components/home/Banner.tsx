21f829ddfa1f41e9e8a52892b5831b4329f4d7f8
Quoc Cuong <cuongnhq@nal.vn>
2025-07-06 13:42:55 +0700

    Feature 551556 fix image banner and bottom banner

--- src/components/home/Banner.tsx
+++ src/components/home/Banner.tsx
@@ -10,13 +10,24 @@ const Banner = () => {
     <div className="relative h-full w-full rounded-lg">
       <div className="absolute inset-0 rounded-lg bg-gradient-to-b" />
       <Image
+        src="/assets/images/banner-mb.svg"
+        width={0}
+        height={0}
+        sizes="100vw"
+        className="h-full w-full rounded-lg object-cover md:hidden"
+        alt="Home Banner"
+      />
+
+      {/* Desktop Image */}
+      <Image
         src="/assets/images/home-banner.png"
         width={0}
         height={0}
         sizes="100vw"
-        className="h-[8rem] w-full rounded-lg object-cover md:h-full"
+        className="hidden h-[8rem] w-full rounded-lg object-cover md:block md:h-full "
         alt="Home Banner"
       />
+
       <div className="absolute left-8 top-1/2 -translate-y-1/2">
         <div className="flex flex-col gap-0 md:gap-2">
           <h3 className="text-base font-medium leading-[1.6rem] text-primary-30 md:text-[2.5rem] md:leading-[3rem]">
