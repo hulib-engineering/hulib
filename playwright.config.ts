627588a08d98671de19a584be8b8254af115ae6b
JtotheDoubleO <joo291998@gmail.com>
2025-07-13 05:23:20 +0700

    fix(713678): updated checkly configuration and use playwright baseURl in Checkly

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,4 +1,3 @@
-/* eslint-disable import/no-extraneous-dependencies */
 import { defineConfig, devices } from '@playwright/test';
 
 // Use process.env.PORT by default and fallback to port 3000
@@ -6,9 +5,6 @@ const PORT = process.env.PORT || 3000;
 
 // Set webServer.url and use.baseURL with the location of the WebServer respecting the correct set port
 const baseURL = `http://localhost:${PORT}`;
-
-// *.check.spec.ts files use ENVIRONMENT_URL instead of baseURL
-process.env.ENVIRONMENT_URL = baseURL;
 
 /**
  * See https://playwright.dev/docs/test-configuration.
