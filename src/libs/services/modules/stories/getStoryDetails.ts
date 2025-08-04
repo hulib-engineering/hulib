39434bfa557b1b0b634de34ca2708a582b920f51
JtotheDoubleO <joo291998@gmail.com>
2025-08-04 19:59:30 +0700

    fix(857976): fixed mobile UI, added bilingual phrases and added opening chat

--- src/libs/services/modules/stories/getStoryDetails.ts
+++ src/libs/services/modules/stories/getStoryDetails.ts
@@ -1,17 +1,17 @@
 import type { BaseQueryFn, EndpointBuilder } from '@reduxjs/toolkit/query';
 
-import type { Story, StoryDetailsParams } from './storiesType';
+import type { Story } from './storiesType';
 
 const getStoryDetails = (build: EndpointBuilder<BaseQueryFn, string, string>) =>
-  build.query<Story, StoryDetailsParams>({
-    query: ({ id }) => ({
-      url: `stories/${id || 0}`,
+  build.query<Story, number>({
+    query: id => ({
+      url: `stories/${id}`,
     }),
     serializeQueryArgs: ({ endpointName, queryArgs }) => {
-      return `${endpointName}-${queryArgs.id || 0}`;
+      return `${endpointName}-${queryArgs}`;
     },
-    providesTags: (_result, _error, { id }) => [
-      { type: 'Story' as const, id: id || 0 },
+    providesTags: (_result, _error, id) => [
+      { type: 'Story' as const, id },
     ],
   });
 
