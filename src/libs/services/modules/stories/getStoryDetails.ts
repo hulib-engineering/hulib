f8ddb3da01c5926dbf216b8421f48f7d366815a5
JtotheDoubleO <joo291998@gmail.com>
2025-08-04 20:01:03 +0700

    Revert "fix(857976): fixed mobile UI, added bilingual phrases and added opening chat"
    This reverts commit 874062f51b0874cfec2d362375a2eff19f99a95d.

--- src/libs/services/modules/stories/getStoryDetails.ts
+++ src/libs/services/modules/stories/getStoryDetails.ts
@@ -1,17 +1,17 @@
 import type { BaseQueryFn, EndpointBuilder } from '@reduxjs/toolkit/query';
 
-import type { Story } from './storiesType';
+import type { Story, StoryDetailsParams } from './storiesType';
 
 const getStoryDetails = (build: EndpointBuilder<BaseQueryFn, string, string>) =>
-  build.query<Story, number>({
-    query: id => ({
-      url: `stories/${id}`,
+  build.query<Story, StoryDetailsParams>({
+    query: ({ id }) => ({
+      url: `stories/${id || 0}`,
     }),
     serializeQueryArgs: ({ endpointName, queryArgs }) => {
-      return `${endpointName}-${queryArgs}`;
+      return `${endpointName}-${queryArgs.id || 0}`;
     },
-    providesTags: (_result, _error, id) => [
-      { type: 'Story' as const, id },
+    providesTags: (_result, _error, { id }) => [
+      { type: 'Story' as const, id: id || 0 },
     ],
   });
 
