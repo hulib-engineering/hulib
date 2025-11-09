66e5e8f78b42c5783e4f8da329faf5c62566e45c
JtotheDoubleO <joo291998@gmail.com>
2025-11-09 13:09:29 +0700

    fix(993796): fixed moderation module endpoints

--- src/libs/services/modules/moderation/banUser.ts
+++ src/libs/services/modules/moderation/banUser.ts
@@ -3,9 +3,9 @@ import type { BaseQueryFn, EndpointBuilder } from '@reduxjs/toolkit/query';
 import type { ModerationHistory } from '@/libs/services/modules/moderation/type';
 
 export default (build: EndpointBuilder<BaseQueryFn, string, string>) =>
-  build.mutation<ModerationHistory, { userId: number }>({
+  build.mutation<ModerationHistory, { userId: number; reportId: number }>({
     query: payload => ({
-      url: '/moderation/ban',
+      url: '/moderations/ban',
       method: 'POST',
       body: payload,
     }),
