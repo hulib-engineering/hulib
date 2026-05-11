b7c8cd06ff8d4bcd8dfb3cec2c01a52df039185a
VuHoaBinh <hoabinh.vippro63@gmail.com>
2026-05-12 01:10:29 +0700

    fix: 440_update_hero_banner

--- src/layouts/index/Banner.tsx
+++ src/layouts/index/Banner.tsx
@@ -1,49 +1,34 @@
 'use client';
 
 import Image from 'next/image';
-import { useTranslations } from 'next-intl';
 import React from 'react';
 
-import { mergeClassnames } from '@/components/core/private/utils';
-
 export default function Banner() {
-  const t = useTranslations('Home');
-
   return (
-    <div className="relative size-full md:rounded-2xl">
-      <Image
-        src="/assets/images/banner-mb.svg"
-        width={686}
-        height={320}
-        className="size-full object-cover md:hidden"
-        alt="Home Banner"
-      />
-      {/* Desktop Image */}
-      <Image
-        src="/assets/images/pc-home-banner.jpg"
-        width={686}
-        height={320}
-        className="hidden h-80 w-full rounded-2xl object-cover md:block"
-        alt="Home Banner"
-      />
+    <div className="relative left-1/2 -mt-4 mb-2 flex w-screen items-center justify-center -translate-x-1/2 sm:-mt-6 sm:mb-3 md:-mt-8 md:mb-4">
+      {/* Mobile Banner */}
+      <div className="relative h-20 w-full sm:hidden">
+        <Image
+          src="/assets/images/banner-mb.jpg"
+          alt="Home Banner Mobile"
+          fill
+          className="object-cover object-center"
+          sizes="100vw"
+          priority
+        />
+      </div>
 
-      <div
-        className={mergeClassnames(
-          'absolute bottom-0 left-0 h-[140px] w-full bg-gradient-to-b from-transparent to-[#F9F9F9]',
-          'md:h-52 md:rounded-2xl',
-        )}
-      />
-
-      <div className="absolute bottom-0 left-0 md:top-1/2 md:-translate-y-1/2">
-        <div className="flex flex-col p-4 md:gap-2 md:p-8">
-          <h1 className="font-medium leading-[1.6rem] text-primary-30 md:text-[2.5rem] md:leading-[3rem]">
-            {t('banner.title')}
-          </h1>
-          <h5 className="w-64 text-sm font-medium leading-6 text-primary-30 md:w-[27.5rem] md:text-2xl md:leading-8">
-            {t('banner.sub_title')}
-          </h5>
-        </div>
+      {/* Desktop Banner */}
+      <div className="relative hidden h-[94px] w-full sm:block">
+        <Image
+          src="/assets/images/banner-new.png"
+          alt="Home Banner"
+          fill
+          className="object-cover object-center"
+          sizes="100vw"
+          priority
+        />
       </div>
     </div>
   );
-};
+}
