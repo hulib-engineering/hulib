2155ebbb28229ce4342947043828f44b1b0de45c
JtotheDoubleO <external-blockchain@tradible.io>
2026-05-23 04:31:00 +0700

    fix: fixed homepage width on extra large screen and updated copyright to @2026

--- src/app/[locale]/(auth)/home/page.tsx
+++ src/app/[locale]/(auth)/home/page.tsx
@@ -19,42 +19,8 @@ export async function generateMetadata({
 }
 
 export default function Index() {
-  // const userInfo = useAppSelector(state => state.auth.userInfo);
-  // const [roleUser, setRoleUser] = useState<Role | null>(null);
-
-  // useEffect(() => {
-  //   if (userInfo) {
-  //     setRoleUser(userInfo.role?.id);
-  //   }
-  // }, [userInfo]);
-
-  // if (!roleUser) {
-  //   return (
-  //     <div className="flex h-screen items-center justify-center">
-  //       <Loader />
-  //     </div>
-  //   );
-  // }
-
-  // if (roleUser === Role.ADMIN) {
-  //   return (
-  //     <AdminLayout className="h-full bg-neutral-98">
-  //       <div className="flex flex-1 items-center justify-center">
-  //         <div className="text-center">
-  //           <div className="mb-2 flex justify-center gap-1">
-  //             <span className="text-2xl">💕</span>
-  //           </div>
-  //           <p className="text-lg font-medium text-neutral-20">
-  //             You have a warm heart
-  //           </p>
-  //         </div>
-  //       </div>
-  //     </AdminLayout>
-  //   );
-  // }
-
   return (
-    <div className="mx-auto w-full bg-neutral-98 py-6 lg:w-5/6 lg:py-8">
+    <div className="mx-auto w-full bg-neutral-98 py-6 xl:w-[1216px] xl:py-8">
       <div className="flex flex-col gap-6">
         <Banner />
         <NewestStoryList />
