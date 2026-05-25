31d956cd80895257392d7db570fa34d1785ccf4a
JtotheDoubleO <joo291998@gmail.com>
2026-05-25 23:21:00 +0700

    fix: inject NEXT_PUBLIC env vars into Storybook browser bundle via webpack DefinePlugin

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,5 @@
 import type { StorybookConfig } from '@storybook/nextjs';
+import webpack from 'webpack';
 
 process.env.NEXT_PUBLIC_REACT_APP_BACKEND_VERSION = process.env.NEXT_PUBLIC_REACT_APP_BACKEND_VERSION ?? 'v1';
 process.env.NEXT_PUBLIC_REACT_APP_BACKEND_ENDPOINT = process.env.NEXT_PUBLIC_REACT_APP_BACKEND_ENDPOINT ?? 'http://localhost:3001';
@@ -29,6 +30,25 @@ const config: StorybookConfig = {
   },
   core: {
     disableTelemetry: true,
+  },
+  webpackFinal: async (config) => {
+    config.plugins?.push(
+      new webpack.DefinePlugin({
+        'process.env.NEXT_PUBLIC_REACT_APP_BACKEND_VERSION': JSON.stringify(process.env.NEXT_PUBLIC_REACT_APP_BACKEND_VERSION),
+        'process.env.NEXT_PUBLIC_REACT_APP_BACKEND_ENDPOINT': JSON.stringify(process.env.NEXT_PUBLIC_REACT_APP_BACKEND_ENDPOINT),
+        'process.env.NEXT_PUBLIC_REACT_APP_BACKEND_SOCKET_ENDPOINT': JSON.stringify(process.env.NEXT_PUBLIC_REACT_APP_BACKEND_SOCKET_ENDPOINT),
+        'process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID': JSON.stringify(process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID),
+        'process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID': JSON.stringify(process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID),
+        'process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY': JSON.stringify(process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY),
+        'process.env.NEXT_PUBLIC_CONTACT_EMAIL': JSON.stringify(process.env.NEXT_PUBLIC_CONTACT_EMAIL),
+        'process.env.NEXT_PUBLIC_CONTACT_PHONE_NUMBER': JSON.stringify(process.env.NEXT_PUBLIC_CONTACT_PHONE_NUMBER),
+        'process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME': JSON.stringify(process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME),
+        'process.env.NEXT_PUBLIC_UPLOAD_PRESET': JSON.stringify(process.env.NEXT_PUBLIC_UPLOAD_PRESET),
+        'process.env.NEXT_PUBLIC_AGORA_APP_ID': JSON.stringify(process.env.NEXT_PUBLIC_AGORA_APP_ID),
+        'process.env.NEXT_PUBLIC_APP_URL': JSON.stringify(process.env.NEXT_PUBLIC_APP_URL),
+      }),
+    );
+    return config;
   },
 };
 
