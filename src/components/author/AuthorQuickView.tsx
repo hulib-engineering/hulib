4a838bb50a55a114dddb39f4c0d136d3ecdd5dff
thedaythatlast <dkn.010101@gmail.com>
2026-08-09 06:10:51 +0700

    feat: add another param pass to booking page & change API query & finish Topics component hover card

--- src/components/author/AuthorQuickView.tsx
+++ src/components/author/AuthorQuickView.tsx
@@ -11,10 +11,10 @@ import AuthorBasicInfo from './AuthorBasicInfo';
 // import type { AuthorBasicInfoProps } from './private/types';
 import Section from '@/components/Section';
 // import { Chip } from '@/components/core/chip/Chip';
-// import { getTopicBadgeClasses } from '@/features/admin/utils/getTopicBadgeClasses';
+import { getTopicBadgeClasses } from '@/features/admin/utils/getTopicBadgeClasses';
 import { useGetHuberStoriesQuery } from '@/libs/services/modules/huber';
 import type { User } from '@/features/users/types';
-// import type { Topic, TopicResponse } from '@/libs/services/modules/topics/topicType';
+import type { Topic } from '@/libs/services/modules/topics/topicType';
 import type { GetHuberStoriesResponse, Story } from '@/libs/services/modules/stories/storiesType';
 
 // TODO: Integrate API response for the Topics component once the 'topics' values are fixed in the BE - (if it's supposed to be removed then
@@ -28,26 +28,28 @@ function MaturingExperiences({ bio }: { bio: string | null }) {
   );
 }
 
-/* function Topics({ stories }: any) {
+function Topics({ topics }: { topics: Topic[] }) {
   const t = useTranslations('Schedule.HoverCard');
 
   return (
     <Section title={t('topics')} row>
-      {topics?.map((t: any) => (
-        <Chip
-          key={t?.topicId}
-          as="span"
-          className={mergeClassnames(
-            'h-full rounded-2xl border p-2 text-xs font-medium leading-[14px]',
-            getTopicBadgeClasses(t?.topic.color),
-          )}
-        >
-          {t?.topic.name}
-        </Chip>
-      ))}
+      <div className="flex flex-wrap gap-2">
+        {topics?.map((t: Topic) => (
+          <div
+            key={t?.id}
+            className={mergeClassnames(
+              'py-2 px-3 rounded-2xl border',
+              'text-xs font-medium leading-[14px]',
+              getTopicBadgeClasses(t?.color),
+            )}
+          >
+            {t?.name}
+          </div>
+        ))}
+      </div>
     </Section>
   );
-} */
+}
 
 /* function About() {
   const t = useTranslations('Schedule.HoverCard');
@@ -89,9 +91,11 @@ function Stories({ stories }: { stories: GetHuberStoriesResponse }) {
     </Section>
   );
 }
-// TODO: remove this query, use a gethuberinfo at the ancestor component instead
+
 export default function AuthorQuickView({ humanBook }: { humanBook: User }) {
   // 2nd API call - as the API call on parent component (which gets user's info) doesn't return list of stories (or at least their titles)
+  // | remove if the 1st API call return response of stories list (or at least their titles)
+
   // Do add 'isLoading' to const {} if need to add loading for the hover card
   const { data: storiesList } = useGetHuberStoriesQuery(
     { huberId: humanBook.id, publishedOnly: true },
@@ -99,13 +103,12 @@ export default function AuthorQuickView({ humanBook }: { humanBook: User }) {
   );
 
   return (
-    // (remove the first line of css) <- if somebody decided to make a hover card and wrap it around this component:
     // (remove 'hidden') <- for easier debugging
     <div className="absolute z-50 box-border flex hidden
       w-[420px] flex-col items-start gap-5 rounded-2xl
       border-2 border-[#0858FA] bg-white p-5
-      shadow-[0px_0px_4px_rgba(15,15,16,0.06),0px_4px_5px_rgba(28,30,33,0.1)] -translate-y-8 group-hover:flex max-lg:right-1/2 max-lg:top-full max-lg:mt-6
-      lg:left-full lg:ml-2"
+      shadow-[0px_0px_4px_rgba(15,15,16,0.06),0px_4px_5px_rgba(28,30,33,0.1)] -translate-y-8 group-hover:flex
+      max-lg:right-1/2 max-lg:top-full max-lg:mt-6 lg:left-full lg:ml-2"
     >
       <AuthorBasicInfo
         avatarSize="2xl"
@@ -113,7 +116,7 @@ export default function AuthorQuickView({ humanBook }: { humanBook: User }) {
         stories={storiesList}
       />
       <MaturingExperiences bio={humanBook.bio} />
-      {/* <Topics stories={storiesList} /> */}
+      <Topics topics={humanBook.sharingTopics} />
       {/* <About /> */}
       <Stories stories={storiesList} />
     </div>
