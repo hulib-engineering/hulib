01d8b0caac75b1026f758a60f8032a0365b5812c
thedaythatlast <dkn.010101@gmail.com>
2026-08-09 08:19:25 +0700

    fix: responsive hover card

--- src/components/author/AuthorQuickView.tsx
+++ src/components/author/AuthorQuickView.tsx
@@ -28,6 +28,7 @@ function MaturingExperiences({ bio }: { bio: string | null }) {
   );
 }
 
+// Design note: no limit to the number of Topics - "as the user won't have so much free time to choose so many of them anyway", said the designer
 function Topics({ topics }: { topics: Topic[] }) {
   const t = useTranslations('Schedule.HoverCard');
 
@@ -74,11 +75,12 @@ function About() {
   );
 } */
 
+// Design note: only 2 stories will be shown at max -- according to the designer
 function Stories({ stories }: { stories: GetHuberStoriesResponse }) {
   const t = useTranslations('Schedule.HoverCard');
   return (
     <Section title={t('stories')}>
-      {stories?.data?.map((story: Story) => (
+      {stories?.data?.slice(0, 2).map((story: Story) => (
         <div
           key={story.id}
           className="box-border flex w-full items-center gap-2 rounded-lg border border-[#C7C9CB] bg-[#F0F5FF] p-2"
@@ -105,11 +107,12 @@ export default function AuthorQuickView({ humanBook }: { humanBook: User }) {
 
   return (
     // (remove 'hidden') <- for easier debugging
+    //
     <div className="absolute z-50 box-border flex hidden
       w-[420px] flex-col items-start gap-5 rounded-2xl
       border-2 border-[#0858FA] bg-white p-5
       shadow-[0px_0px_4px_rgba(15,15,16,0.06),0px_4px_5px_rgba(28,30,33,0.1)] -translate-y-8 group-hover:flex
-      max-lg:right-1/2 max-lg:top-full max-lg:mt-6 lg:left-full lg:ml-2"
+      max-lg:top-full max-lg:mt-8 lg:left-full lg:ml-2"
     >
       <AuthorBasicInfo
         avatarSize="2xl"
