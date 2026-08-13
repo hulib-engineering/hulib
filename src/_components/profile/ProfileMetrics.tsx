6cc7cb76630dfebe47b5693f7df9a6a7fdf5ee95
Nhi Le <ilenhi2710@gmail.com>
2026-08-13 23:44:31 +0700

    fix: update metric number

--- src/_components/profile/ProfileMetrics.tsx
+++ src/_components/profile/ProfileMetrics.tsx
@@ -16,14 +16,14 @@ function MetricItem({
   subtitle,
 }: {
   icon: React.ReactNode;
-  value: number;
+  value: number | string;
   title: string;
   subtitle?: string;
 }) {
   return (
     <div className="flex flex-1 items-center gap-1 rounded-lg px-1 lg:gap-2 lg:px-2">
       <div className="hidden size-10 shrink-0 items-center justify-center lg:flex">{icon}</div>
-      <span className="text-2xl font-medium text-neutral-10 lg:text-4xl">{value > 0 ? value : '-'}</span>
+      <span className="text-2xl font-medium text-neutral-10 lg:text-4xl">{value}</span>
       <div className="flex flex-col lg:gap-1">
         <span className="text-xs font-medium text-neutral-10 lg:text-sm">{title}</span>
         {subtitle && <span className="text-[10px] text-neutral-10 lg:text-xs">{subtitle}</span>}
@@ -48,21 +48,21 @@ export default function ProfileMetrics({
     <div className="grid grid-cols-2 gap-y-4 py-2 lg:flex lg:items-center lg:justify-between lg:gap-4 lg:border-t lg:border-neutral-variant-90 lg:py-4">
       <MetricItem
         icon={<BooksIcon size={40} className="text-primary-50" />}
-        value={storiesCount}
+        value={storiesCount ?? '-'}
         title={t('stories_title')}
         subtitle={t('stories_sub')}
       />
       <Divider />
       <MetricItem
         icon={<VideoCameraIcon size={40} className="text-primary-50" />}
-        value={conversationsCount}
+        value={conversationsCount ?? '-'}
         title={t('conversations_title')}
         subtitle={t('conversations_sub')}
       />
       <Divider />
       <MetricItem
         icon={<HeartIcon size={40} className="text-pink-40" weight="fill" />}
-        value={rating ?? 0}
+        value={rating && rating > 0 ? rating : '-'}
         title={t('rating_title')}
         subtitle={t('rating_sub', { count: ratingCount })}
       />
