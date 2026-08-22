21f4a3989aa22eedfbd2d8ed140275ff9538ef57
thedaythatlast <dkn.010101@gmail.com>
2026-08-22 14:43:18 +0700

    fix: makes popover stop being hidden due to whatever error occured by rtk query

--- src/app/[locale]/(auth)/_components/Header.tsx
+++ src/app/[locale]/(auth)/_components/Header.tsx
@@ -74,7 +74,7 @@ const Header = () => {
   const currentPathname = usePathname();
   const user = useAppSelector(state => state.auth.userInfo);
 
-  const { data, isLoading, error } = useGetUnseenNotificationCountQuery();
+  const { data, isLoading } = useGetUnseenNotificationCountQuery();
   const { data: conversations = [] } = useGetConversationContactsQuery(
     undefined,
     {
@@ -245,7 +245,7 @@ const Header = () => {
                     </>
                   )}
                 </Popover>
-                {!isLoading && !error && <NotificationPopover unreadNotifCount={data ? data.unseenCount : 0} />}
+                {!isLoading && <NotificationPopover unreadNotifCount={data ? data.unseenCount : 0} />}
                 <div className="ml-2">
                   <AvatarPopover />
                 </div>
