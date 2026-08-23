abf716526859f2506c3314a0a475814f206ea52d
thedaythatlast <dkn.010101@gmail.com>
2026-08-23 20:55:07 +0700

    fix: minor meeting component issue

--- src/app/[locale]/(auth)/explore-story/[id]/_components/StorySidePanel.tsx
+++ src/app/[locale]/(auth)/explore-story/[id]/_components/StorySidePanel.tsx
@@ -59,13 +59,13 @@ function BookMeeting({ handleBookingClick, userId }: { handleBookingClick: () =>
   const { data: bookedSessionsList, isLoading }
   = useGetHuberBookedSessionsQuery({ id: userId }, { skip: !userId }); // replace with a var of user's number of booked sessions if BE added that
 
-  const max_lg = '';// 'max-lg:absolute max-[425px]:bottom-10 max-lg:bottom-20 max-lg:left-0 z-[5] max-lg:mx-4 p-4';
+  const max_lg = '';// 'max-lg:absolute max-[425px]:bottom-10 max-lg:bottom-20 max-lg:left-0 z-[5] max-lg:mx-4';
   const lg = 'lg:p-5';
 
   return (
     <div
       className={mergeClassnames(
-        'w-auto flex flex-col items-center gap-5 overflow-hidden rounded-2xl bg-white shadow-sm border-2 border-primary-70',
+        'w-auto flex flex-col items-center gap-5 overflow-hidden rounded-2xl bg-white shadow-sm border-2 border-primary-70 p-4',
         max_lg,
         lg,
       )}
@@ -79,7 +79,7 @@ function BookMeeting({ handleBookingClick, userId }: { handleBookingClick: () =>
       >
         <span className="mt-1">{t('book_a_meeting')}</span>
       </Button>
-      {!disabledCondition ? <></>
+      {disabledCondition ? <></>
         : (
             <p className="text-center text-xs leading-[14px] text-neutral-20">
               {t('booking_count', { bookingCount: `${(disabledCondition || isLoading) ? 0 : bookedSessionsList?.length}` })}
