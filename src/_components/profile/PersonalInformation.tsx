2ed4fff045186ee403fdf33cc60165c61185e094
thedaythatlast <dkn.010101@gmail.com>
2026-09-19 10:46:57 +0700

    fix: remove fields clearing when form hasn't been submitted

--- src/_components/profile/PersonalInformation.tsx
+++ src/_components/profile/PersonalInformation.tsx
@@ -465,23 +465,15 @@ export default function PersonalInformation({ data }: IProfileFormProps) {
   });
 
   const birthday = watch('birthday');
-  const [prevBirthday, setPrevBirthday] = useState(birthday);
 
   useEffect(() => {
-    if (!birthday || birthday === prevBirthday) {
+    if (!birthday) {
       return;
     }
-    setPrevBirthday(birthday);
-
     const age = calculateAge(birthday);
     const underGuard = age < 18;
     setValue('isUnderGuard', underGuard, { shouldDirty: true, shouldValidate: true });
-
-    if (!underGuard) {
-      setValue('parentPhoneNumber', null, { shouldDirty: true, shouldValidate: true });
-      setValue('parentEmail', '', { shouldDirty: true, shouldValidate: true });
-    }
-  }, [birthday, prevBirthday, setValue]);
+  }, [birthday, setValue]);
 
   const handleUpdate = handleSubmit(async (values: TProfileForm) => {
     try {
